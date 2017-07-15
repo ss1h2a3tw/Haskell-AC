87672dcf07a1d470991572c877f4da97fa951af5
ss1h2a3tw <ss1ha3tw@gmail.com>
2017-07-16 02:17:07 +0800

    declarative AC

---
+++ main-new.hs
@@ -0,0 +1,113 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GADTs #-}
+
+import Data.Char
+import Data.Maybe
+import Data.Map as Map
+import qualified Data.Text as T
+import qualified Data.Array as A
+
+type TParent = (Trie, Char)
+data Trie = Trie Bool (Maybe TParent) (Map Char Trie) deriving Eq
+instance Show Trie where
+  show (Trie hit Nothing m) = "Root Node [" ++ showchild m ++ "]"
+    where showchild cm = Map.foldl (\pre now->(pre++" "++(show now))) "" cm
+  show (Trie hit (Just (_,c)) m) = "From :" ++ [c] ++ "[" ++ showchild m ++ "]"
+    where showchild cm = Map.foldl (\pre now->(pre++" "++(show now))) "" cm
+
+realSingleTrie :: (Maybe TParent) -> String -> Trie
+realSingleTrie par [] = Trie True par empty
+realSingleTrie par (c:cs) = Trie False par . singleton c $
+  realSingleTrie (Just (realSingleTrie par (c:cs), c)) cs
+
+singleTrie :: String -> Trie
+singleTrie s = realSingleTrie Nothing s
+
+getParent :: Trie -> Maybe Trie
+getParent (Trie _ Nothing _) = Nothing
+getParent (Trie _ (Just (p,_)) _) = Just p
+
+modifyParent :: (Maybe TParent) -> Trie -> Trie
+modifyParent p (Trie hit _ m) = ret
+  where
+  ret = Trie hit p $
+    mapWithKey f m
+  f k c = modifyParent (Just (ret,k)) c
+
+realMergeTrie :: (Maybe TParent) -> Trie -> Trie -> Trie
+realMergeTrie par (Trie isA _ ma) (Trie isB _ mb) = ret
+  where
+  ret = Trie (isA || isB) par $ foldlWithKey f ma mb
+  -- self = realMergeTrie par (Trie isA undefined ma) (Trie isB undefined mb)
+  f :: Map Char Trie -> Char -> Trie -> Map Char Trie
+  f m k b
+    | isNothing $ Map.lookup k m = insert k (modifyParent (Just (ret,k)) b) m
+    | otherwise = insert k (realMergeTrie (Just (ret, k)) (m ! k) b) m
+
+mergeTrie :: Trie -> Trie -> Trie
+mergeTrie a b = realMergeTrie Nothing a b
+
+constructTrie :: [String] -> Trie
+constructTrie [] = Trie False Nothing empty
+constructTrie (x:xs) = mergeTrie (constructTrie xs) (singleTrie x)
+
+getNodeTrie :: String -> Trie -> Maybe Trie
+getNodeTrie [] t = Just t
+getNodeTrie (c:cs) (Trie _ _ m)
+  | isNothing $ Map.lookup c m = Nothing
+  | otherwise = getNodeTrie cs $ m ! c
+
+inTrie :: String -> Trie -> Bool
+inTrie cs t = isHit
+  where
+  isHit
+    | isNothing (getNodeTrie cs t) = False
+    | otherwise =  (\(Trie x _ _)->x) $ fromJust $ getNodeTrie cs t
+
+jumpTrie :: Trie -> Char -> Trie
+jumpTrie (Trie hit Nothing m) c
+  | isNothing $ Map.lookup c m = self
+  | otherwise = m ! c
+  where
+  self = Trie hit Nothing m
+jumpTrie (Trie hit par m) c
+  | isJust $ Map.lookup c m = m ! c
+  | otherwise = jumpTrie (failTrie self) c
+  where
+  self = Trie hit par m
+
+failTrie :: Trie -> Trie
+failTrie (Trie hit Nothing m) = Trie hit Nothing m
+failTrie (Trie hit (Just (par, parc)) m)
+  | isroot par = par
+  | otherwise =  jumpTrie (failTrie par) parc
+  where
+  isroot (Trie _ p _) = isNothing p
+
+hitfailTrie :: Trie -> Trie
+hitfailTrie (Trie hit Nothing m) = Trie hit Nothing m
+hitfailTrie (Trie hit (Just (par, parc)) m)
+  | isroot par = par
+  | ishit $ failTrie self = failTrie self
+  | otherwise = hitfailTrie (failTrie self)
+  where
+  self = Trie hit (Just (par, parc)) m
+  isroot (Trie _ p _) = isNothing p
+  ishit (Trie h _ _) = h
+
+type AC = Trie
+
+str :: Trie->String
+str (Trie _ Nothing _) = []
+str (Trie _ (Just (par,c)) _) = str par ++ [c]
+
+buildAC = constructTrie
+
+runAC :: AC -> Char -> AC
+runAC a c
+  | isJust $ Map.lookup c sub = sub ! c
+  | isroot a = a
+  | otherwise = runAC (failTrie a) c
+    where
+    Trie _ _ sub = a
+    isroot (Trie _ p _) = isNothing p
